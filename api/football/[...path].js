16459972910808a0b26cd7acff8657453cf8c7bf
Richard Sneider Malagón Conde <claudiasii3@udca.edu.co>
2026-07-01 14:50:10 -0500

    fix: corrige sintaxis CommonJS en funcion serverless de Vercel

--- api/football/[...path].js
+++ api/football/[...path].js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     const { path = [], ...query } = req.query;
     const pathStr = Array.isArray(path) ? path.join('/') : (path || '');
 
@@ -19,4 +19,4 @@ export default async function handler(req, res) {
     } catch {
         res.status(500).json({ error: 'Proxy error' });
     }
-}
+};
